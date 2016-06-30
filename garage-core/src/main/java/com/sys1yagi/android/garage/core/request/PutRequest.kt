3c45a25049e19575d86e19a408b2d267459f1053
sys1yagi <sylc.yagi@gmail.com>
2016-06-30 23:45:54 +0900

    Update kotlin to 1.0.3.

---
+++ garage-core/src/main/java/com/sys1yagi/android/garage/core/request/PutRequest.kt
@@ -0,0 +1,50 @@
+package com.sys1yagi.android.garage.core.request
+
+import android.util.Log
+import com.sys1yagi.android.garage.core.GarageClient
+import com.sys1yagi.android.garage.core.config.RequestConfiguration
+import okhttp3.Call
+import okhttp3.Request
+import okhttp3.RequestBody
+
+open class PostRequest(private val path: Path, private val requestBody: RequestBody, private val config: RequestConfiguration, val requestPreparing: (Request.Builder) -> Request.Builder = { it }) : GarageRequest() {
+
+    var parameter: Parameter? = null
+
+    override fun url(): String {
+        with(config) {
+            return ("${scheme.value}://${endpoint}:${customPort ?: scheme.port}/${path.to()}" + (parameter?.let { "?${it.build()}" } ?: "")).apply {
+                if (config.isDebugMode) {
+                    Log.d(GarageClient.TAG, "POST:$this")
+                }
+            }
+        }
+    }
+
+    override fun newCall(requestProcessing: (Request.Builder) -> Request.Builder): Call {
+        return config.client.newCall(
+                requestProcessing(Request.Builder())
+                        .url(url())
+                        .post(requestBody)
+                        .build())
+    }
+
+    override fun execute(success: (GarageResponse) -> Unit, failed: (GarageError) -> Unit) {
+        val call = newCall(requestPreparing)
+        try {
+            val response = call.execute()
+            success.invoke(GarageResponse(call, response))
+        } catch(e: Exception) {
+            failed.invoke(
+                    GarageError(e).apply {
+                        this.call = call
+                        if (config.isDebugMode) {
+                            e.printStackTrace()
+                        }
+                    }
+            )
+        }
+    }
+
+    override fun requestTime() = System.currentTimeMillis()
+}
